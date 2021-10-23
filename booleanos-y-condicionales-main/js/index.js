242623f32e9d66e021d81f2a52b970b0aa45f720
Diel2394 <80163824+Diel2394@users.noreply.github.com>
2021-10-24 03:01:42 +0700

    variables y booleanos

---
+++ booleanos-y-condicionales-main/js/index.js
@@ -0,0 +1,8 @@
+alert("hola mundo");
+console.log(2 > 4); //false
+console.log(2 == 2); //false
+console.log("42" == 42); //true
+console.log("42" === 42); //false
+let resultado = 5 === 4;
+console.log(resultado); //false
+console.log(2 == 3);
